4802bae404778651780d270a39cd077f604825d6
Kauan Ivo <kauanivo09@gmail.com>
2026-06-15 11:51:30 -0300

    feat: configura PostgreSQL e entidade usuario

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.core.database import engine
 from app.domain.base import Base
 from app.domain.usuario import Usuario
+from app.api.auth import router as auth_router
 
 app = FastAPI(
     title="Raízes do Nordeste API",
